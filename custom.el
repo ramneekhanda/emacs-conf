1a538b603ce7fe28c6da7c598c6c5e363571b74c
Ramneek Handa <ramneekhanda@gmail.com>
2021-07-27 07:20:04 +0800

    first commit

---
+++ custom.el
@@ -0,0 +1,62 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
+ '(bison-decl-token-column 24)
+ '(bison-decl-type-column 8)
+ '(custom-safe-themes
+   '("a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "fe2539ccf78f28c519541e37dc77115c6c7c2efcec18b970b16e4a4d2cd9891d" "5aef652e40fa5f111e78997285f6e4c892112da0c2f919eb663baaa330a8521f" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "a7b20039f50e839626f8d6aa96df62afebb56a5bbd1192f557cb2efb5fcfb662" "6c386d159853b0ee6695b45e64f598ed45bd67c47f671f69100817d7db64724d" "88f59acbeacefb4998f45126d4d8ae8b2184f2a48753db362a349fd55321c7e1" "1f1b545575c81b967879a5dddc878783e6ebcca764e4916a270f9474215289e5" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "b5803dfb0e4b6b71f309606587dd88651efe0972a5be16ece6a958b197caeed8" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "e6a2832325900ae153fd88db2111afac2e20e85278368f76f36da1f4d5a8151e" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "f302eb9c73ead648aecdc1236952b1ceb02a3e7fcd064073fb391c840ef84bca" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "9f9fc38446c384a4e909b7220d15bf0c152849ef42f5b1b97356448612c77953" "3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" "1f50a7274cd56f28713e1694600ec7b8f2fd1c7d2ef38c5e7378a26931605409" "b7e460a67bcb6cac0a6aadfdc99bdf8bbfca1393da535d4e8945df0648fa95fb" "0b3aee906629ac7c3bd994914bf252cf92f7a8b0baa6d94cb4dfacbd4068391d" "6e14157d0c8857e81035e6c7131dc17e4115b3911c82a1fd32e528aec8e89eab" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" default))
+ '(doom-themes-treemacs-enable-variable-pitch nil)
+ '(doom-themes-treemacs-theme "doom-colors")
+ '(doom-themes-treemacs-variable-pitch-face '(:font-family "consolas"))
+ '(exwm-floating-border-color "#0F1019")
+ '(fci-rule-color "#767676")
+ '(highlight-tail-colors
+   ((("#172219" "color-113" "green")
+     . 0)
+    (("#112226" "color-79" "cyan")
+     . 20)))
+ '(irony-additional-clang-options
+   '("-I/usr/local/include" "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1" "-I/Library/Developer/CommandLineTools/usr/lib/clang/12.0.5/include" "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include" "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks" "-I/Library/Developer/CommandLineTools/usr/include" "-std=c++14") t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#0F1019" "#D85F00"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0F1019" "#79D836"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0F1019" "#767676"))
+ '(objed-cursor-color "#D83441")
+ '(package-selected-packages '(bison-mode))
+ '(pdf-view-midnight-colors (cons "#CEDBE5" "#0D0E16"))
+ '(rustic-ansi-faces
+   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
+ '(vc-annotate-background "#0D0E16")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#79D836")
+    (cons 40 "#98cd39")
+    (cons 60 "#b8c33d")
+    (cons 80 "#D8B941")
+    (cons 100 "#d89b2b")
+    (cons 120 "#d87d15")
+    (cons 140 "#D85F00")
+    (cons 160 "#ba5548")
+    (cons 180 "#9d4b90")
+    (cons 200 "#8041D8")
+    (cons 220 "#9d3ca5")
+    (cons 240 "#ba3873")
+    (cons 260 "#D83441")
+    (cons 280 "#bf444e")
+    (cons 300 "#a7555b")
+    (cons 320 "#8e6568")
+    (cons 340 "#767676")
+    (cons 360 "#767676")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-themes-treemacs-file-face ((t (:inherit font-lock-doc-face :slant normal))))
+ '(doom-themes-treemacs-root-face ((t (:inherit font-lock-string-face)))))
+(put 'customize-group 'disabled nil)
+(put 'customize-variable 'disabled nil)
